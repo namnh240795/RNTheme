a6fd541a727dda9f1b57da9220750a4998954d41
Nguyen Hoai Nam <namnguyen@Nguyens-MacBook-Pro-3.local>
2020-03-14 22:25:44 +0700

    test eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    extends: 'eslint-config-umi',
+    rules: {
+      'no-debugger': 'error',
+      'no-duplicate-imports': 'error',
+      'no-unused-vars': 'error',
+      'no-console': 'warn',
+    },
+  },
 };
